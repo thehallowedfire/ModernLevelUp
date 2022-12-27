8445266fdd11d2740cf04eb179760b51e58f6c52
Vladislav Kondrashov <vlad.a.kondrashov@gmail.com>
2022-12-27 07:31:22 +0400

    Improved MCF_GetSettings function

--- BlizzardStuff/MCF-CharacterFrame.lua
+++ BlizzardStuff/MCF-CharacterFrame.lua
@@ -9,21 +9,27 @@ MCF_DEFAULT_SETTINGS = {
 
 	["statCategoriesCollapsed"] = {false, false, false, false, false, false, false},
 	["statCategoriesCollapsed_2"] = {false, false, false, false, false, false, false},
-}
 
-function MCF_GetSettings(query, id)
-	if (id) then
-		return MCF_SETTINGS[query][id];
-	else
-		return MCF_SETTINGS[query];
-	end
-end
+	["newSetting1"] = true,
+	["newSetting2"] = {"one", "two"},
+}
 
 function MCF_SetSettings(setting, value, id)
 	if (id) then
 		MCF_SETTINGS[setting][id] = value;
 	else
 		MCF_SETTINGS[setting] = value;
+	end
+end
+
+function MCF_GetSettings(query, id)
+	if ( MCF_SETTINGS[query] == nil ) then
+		MCF_SETTINGS[query] = MCF_DEFAULT_SETTINGS[query];
+	end
+	if (id) then
+		return MCF_SETTINGS[query][id];
+	else
+		return MCF_SETTINGS[query];
 	end
 end
 
