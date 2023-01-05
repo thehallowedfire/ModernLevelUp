d49c1114b014219512d447c4567fc84133349a65
Vladislav Kondrashov <vlad.a.kondrashov@gmail.com>
2023-01-05 07:13:27 +0400

    Options frame improvements

--- Options.lua
+++ Options.lua
@@ -9,7 +9,24 @@ function MCF_CreateOptionsFrame()
     MCF.OptionsFrame = CreateFrame("Frame", "MCF_OptionsFrame", nil, "MCF_OptionsFrameTemplate");
 end
 
+-- OPTIONS FRAME FUNCTIONS
+function MCF_OptionsFrame_OnLoad(self)
+    self.name = "Modern Character Frame";
+    InterfaceOptions_AddCategory(self);
+    
+    _G[self:GetName().."SubText"]:SetText(L["MCF_OPTIONS_DESCRIPTION"]);
+    _G[self:GetName().."TacoTipIntegrationTitle"]:SetText(L["MCF_OPTIONS_TT_INTEGRATION_TITLE"]);
+end
+
 -- TACOTIP INTEGRATION ENABLE BUTTON FUNCTIONS
+function MCF_OptionsFrameTTEnableButton_OnLoad(self)
+    self.Text:SetText(L["MCF_OPTIONS_TT_INTEGRATION_ENABLE_LABEL"]);
+    self.tooltip = L["MCF_OPTIONS_TT_INTEGRATION_ENABLE_TOOLTIP"];
+
+    self:RegisterEvent("ADDON_LOADED");
+
+    self:HookScript("OnClick", function(self) MCF_OptionsFrameTTEnableButton_HookOnClick(self); end);
+end
 function MCF_OptionsFrameTTEnableButton_OnEvent(self, event, ...)
     local arg = ...;
 
@@ -26,7 +43,7 @@ function MCF_OptionsFrameTTEnableButton_OnShow(self)
     if ( not IsAddOnLoaded("TacoTip") ) then
         self:Disable();
         self.Text:SetFontObject(GameFontDisable);
-        _G[self:GetParent():GetName().."Title"]:SetText(MCF_OPTIONS_TT_INTEGRATION_TITLE_DISABLED);
+        _G[self:GetParent():GetName().."Title"]:SetText(L["MCF_OPTIONS_TT_INTEGRATION_TITLE_DISABLED"]);
     end
 end
 function MCF_OptionsFrameTTEnableButton_HookOnClick(self)
@@ -45,6 +62,10 @@ function MCF_OptionsFrameTTEnableButton_HookOnClick(self)
 end
 
 -- TACOTIP GEARSCORE TEXT TYPE DROPDOWN MENU FUNCTIONS
+function MCF_OptionsFrameGSTextTypeDropDown_OnLoad(self)
+    self:RegisterEvent("ADDON_LOADED");
+    _G[self:GetName().."Label"]:SetText(L["MCF_OPTIONS_TT_INTEGRATION_GSTYPE_LABEL"]);
+end
 function MCF_OptionsFrameGSTextTypeDropDown_Initialize()
     local selectedValue = UIDropDownMenu_GetSelectedValue(MCF_OptionsFrameTacoTipIntegrationGSStatTypeDropDown);
     local info = UIDropDownMenu_CreateInfo();
@@ -122,6 +143,14 @@ function MCF_OptionsFrameGSTextTypeDropDown_OnClick(self)
 end
 
 -- TACOTIP GEARSCORE COLOR ENABLE BUTTON FUNCTIONS
+function MCF_OptionsFrameGSColorEnableButton_OnLoad(self)
+    self.Text:SetText(L["MCF_OPTIONS_TT_INTEGRATION_COLOR_LABEL"]);
+    self.tooltip = L["MCF_OPTIONS_TT_INTEGRATION_COLOR_TOOLTIP"];
+
+    self:RegisterEvent("ADDON_LOADED");
+
+    self:HookScript("OnClick", function(self) MCF_OptionsFrameGSColorEnableButton_HookOnClick(self); end);
+end
 function MCF_OptionsFrameGSColorEnableButton_OnEvent(self, event, ...)
     local arg = ...;
 
@@ -147,4 +176,35 @@ function MCF_OptionsFrameGSColorEnableButton_HookOnClick(self)
     else
         MCF_SetSettings("TT_IntegrationColorEnabled", false);
     end
+end
+
+-- RESET SETTINGS BUTTON FUNCTIONS
+function MCF_OptionsFrameResetSettingsButton_OnLoad(self)
+    self.text:SetText(L["MCF_OPTIONS_RESET_BUTTON_TEXT"]);
+
+    self.tooltip = L["MCF_OPTIONS_RESET_BUTTON_TOOLTIP"];
+    self:SetWidth(self.text:GetWidth() + 40);
+end
+function MCF_OptionsFrameResetSettingsButton_OnShow(self)
+    self:Enable();
+end
+function MCF_OptionsFrameResetSettingsButton_OnClick(self)
+    StaticPopupDialogs["MCF_RESET_SETTINGS"] = {
+        text = L["MCF_OPTIONS_CONFIRM_RESET"],
+        button1 = "Yes",
+        button2 = "No",
+        OnAccept = function()
+            MCF_ResetSettings();
+            print(L["MCF_OPTIONS_RESETED_MESSAGE"]);
+            self:Disable();
+        end,
+        OnCancel = function()
+            self:Enable();
+        end,
+        timeout = 0,
+        whileDead = true,
+        hideOnEscape = true,
+    }
+
+    StaticPopup_Show("MCF_RESET_SETTINGS", self);
 end
