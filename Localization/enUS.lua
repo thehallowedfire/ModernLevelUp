fcf0a3d2811a7b6b805ff557d348241c7dad3ec7
Vladislav Kondrashov <vlad.a.kondrashov@gmail.com>
2023-01-09 20:58:06 +0400

    Locale updates

--- Localization/enUS.lua
+++ Localization/enUS.lua
@@ -10,4 +10,6 @@ if GetLocale() == ("enUS") then
     L["MLU_LEVEL_UP"] = LEVEL_UP;
     L["MLU_LEVEL_UP_TALENT_MAIN"] = LEVEL_UP_TALENT_MAIN;
     L["MLU_LEVEL_GAINED"] = LEVEL_GAINED;
+    L["MLU_LEVEL_UP_GLYPH1_LINK"] = "New |cffFF4E00|Hgarrmission:MLU:glyphpane|h[Major Glyph]|h|r Available!";
+    L["MLU_LEVEL_UP_GLYPH2_LINK"] = "New |cffFF4E00|Hgarrmission:MLU:glyphpane|h[Minor Glyph]|h|r Available!";
 end
--- Localization/ruRU.lua
+++ Localization/ruRU.lua
@@ -4,4 +4,6 @@ if GetLocale() == "ruRU" then
     L["MLU_LEVEL_UP"] = "Поздравляем, вы достигли |cffFF4E00|Hlevelup:%d:LEVEL_UP_TYPE_CHARACTER|h[%d-го уровня]|h|r!";
     L["MLU_LEVEL_UP_TALENT_MAIN"] = "Доступно новое очко талантов";
     L["MLU_LEVEL_GAINED"] = "%d-го уровня";
+    L["MLU_LEVEL_UP_GLYPH1_LINK"] = "Доступен новый |cffFF4E00|Hgarrmission:MLU:glyphpane|h[большой символ]|h|r!";
+    L["MLU_LEVEL_UP_GLYPH2_LINK"] = "Доступен новый |cffFF4E00|Hgarrmission:MLU:glyphpane|h[малый символ]|h|r!";
 end
