251026e20d4b9f634a85c729dea710a34e06ef6d
Vladislav Kondrashov <vlad.a.kondrashov@gmail.com>
2023-01-10 15:40:37 +0400

    Fixed level up chat message not having link

--- Localization/enUS.lua
+++ Localization/enUS.lua
@@ -7,7 +7,7 @@ end
 setmetatable(L, {__index=defaultFunc});
 
 if GetLocale() == ("enUS") then
-    L["MLU_LEVEL_UP"] = LEVEL_UP;
+    L["MLU_LEVEL_UP"] = "Congratulations, you have reached |cffFF4E00|Hlevelup:%d:LEVEL_UP_TYPE_CHARACTER|h[Level %d]|h|r!";
     L["MLU_LEVEL_UP_TALENT_MAIN"] = LEVEL_UP_TALENT_MAIN;
     L["MLU_LEVEL_GAINED"] = LEVEL_GAINED;
     L["MLU_LEVEL_UP_GLYPH1_LINK"] = "New |cffFF4E00|Hgarrmission:MLU:glyphpane|h[Major Glyph]|h|r Available!";
